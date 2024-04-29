4364f8f013d5e828849548ae5cfbdbd20498bb1f
sampath <sampathbojjaopensrcproject@gmail.com>
2024-04-29 21:16:22 +0530

    basic CRUD operations working fine

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,42 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  let [cname, Setcname] = useState('');
+  let [fname, Setfname] = useState('');
+  let [lname, Setlname] = useState('');
   let [dbData, SetdbData] = useState([]);
 
   useEffect(() => {
     fetchDBData(); // the functionality of this is to get the data from the json and give it to dbData hook as list of objects so that it uses the map function and renders the data...on the output
   }, [dbData])
 
+  let HandleFnameEvent = (fnameevent) => {Setfname(fnameevent.target.value);}
+  let HandleLnameEvent = (lnameevent) => {Setlname(lnameevent.target.value);}
+
   async function fetchDBData() {
     let resp = await axios.get('http://localhost:4001/Candidatenames');
     SetdbData(resp.data);
   }
 
-  let HandleNameEvent = (nameevent) => {Setcname(nameevent.target.value);}
-
   async function EnterData() {
-
-    if(cname == ''){
+    if(fname == '' || lname ==''){
       alert("Please enter all the feilds");
     }
     else{
-      let name = cname;
-      await axios.post('http://localhost:4001/Candidatenames', { name});
-      Setcname('');
+      await axios.post('http://localhost:4001/Candidatenames', {fname, lname});
+      Setfname('');
+      Setlname('');
 
     }
   }
-  let urlHalf = "http://localhost:4001/Candidatenames/"
 
   async function DeleteInd(propVal) {
-
-
-    let url = urlHalf + propVal;
-    // console.log(url)
-    await axios.delete(url)
+    await axios.delete("http://localhost:4001/Candidatenames/" + propVal)
   }
 
   async function ModifyInd(modifiedpropVal) {
-    let url = urlHalf+ modifiedpropVal;
-    await axios.put(url, {"name": "maarindi ggaa"})
+    let firstname = prompt("Enter the firstname");
+    let lastname = prompt("Enter the last name")
+    await axios.put("http://localhost:4001/Candidatenames/"+ modifiedpropVal, {"fname": firstname, "lname":lastname})
   }
 
 
@@ -50,7 +47,8 @@ function App() {
     <>
       <div className='LoginFormStyling mt-5'>
         <div className='d-flex flex-column align-items-center'>
-          <input type="text" placeholder='Enter your name' value={cname} onChange={HandleNameEvent} className='form-control mb-3 HeightAndWidth' />
+          <input type="text" placeholder='Enter your fname' value={fname} onChange={HandleFnameEvent} className='form-control mb-3 HeightAndWidth' />
+          <input type="text" placeholder='Enter your lname' value={lname} onChange={HandleLnameEvent} className='form-control mb-3 HeightAndWidth' />
           <div className='butt'>
             <button className='btn btn-success'>Show</button>
             <button className='btn btn-primary' onClick={EnterData}>Enter</button>
@@ -64,9 +62,9 @@ function App() {
           (d, i) => {
             return (
               <p key={i}>
-                {d.name}
-                <button onClick={() => DeleteInd(d.id)}>X</button>
-                <button onClick={() => ModifyInd(d.id)}>M</button>
+                {d.fname} {d.lname}
+                <button className='btn btn-danger' onClick={() => DeleteInd(d.id)}>X</button>
+                <button className='btn btn-info' onClick={() => ModifyInd(d.id)}>M</button>
               </p>
             )
           }
